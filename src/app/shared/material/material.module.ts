accbb5cf2940f4546a23c4b5a914e5af11da6be6
Vinodh JD <jdvinodh@gmail.com>
2022-06-28 14:00:20 +0530

    removed unwanted modules

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,32 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatToolbarModule,
-    MatFormFieldModule,
     MatIconModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule
+    MatCardModule
   ],
   exports: [
     MatToolbarModule,
-    MatFormFieldModule,
     MatIconModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule
+    MatCardModule
   ],
 })
 export class MaterialModule {}
